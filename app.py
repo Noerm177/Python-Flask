c1663e459adbc0138534056299981d2a5997af6e
Noerm177 <noerm177@gmail.com>
2019-11-11 11:44:33 -0600

    primero

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+from products import products
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"message": "pong!"})
+
+@app.route('/products', methods=['GET'])
+def getPtoducts():
+    return jsonify(products)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
